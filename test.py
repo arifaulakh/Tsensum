c42b26374d4e3e0354ca2bda1f017366b2e15fa2
Plasma-Vortex <howardhalim@gmail.com>
2018-09-15 00:47:44 -0400

    Finish sentiment analysis

--- test.py
+++
@@ -1,7 +0,0 @@
-import indicoio
-indicoio.config.api_key = 'b3962258311d6c73574642b4877813b9'
-
-print(indicoio.sentiment([
-    "I love writing code!",
-    "Alexander and the Terrible, Horrible, No Good, Very Bad Day"
-]))
