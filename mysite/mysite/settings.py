ce217e96f5fb8278cb93efef57fcbf3512b869f2
arifaulakh <aular@utschools.ca>
2018-09-15 13:24:10 -0400

    create search on django

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'search',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+APP_DIRS = True
--- mysite/search/urls.py
+++ mysite/search/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from search import views
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('', views.search_form)
+    ##path('', views.index, name = 'index'),
+    path('search_form/', views.search_form),
+    path('search/', views.search)
 ]
