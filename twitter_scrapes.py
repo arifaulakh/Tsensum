60ea7884907dccb6e74f1fb9dfbb43290ea87b5c
wirooo <willh.lu@gmail.com>
2018-09-15 09:15:37 -0400

    Created Twitter Getter Method

--- twitter_scrapes.py
+++ twitter_scrapes.py
@@ -1,25 +1,43 @@
 import twitter
 import json
 
-# twitter api setup
-consumer_key = "8kpMQ4bOrZyse7UIZwWbnFnkv"
-consumer_secret = "x2mUXcR3CtuYpZ62xnspwUezaqMSPGiy9GKAuSlydkv717h3kD"
-access_token = "3044703939-YbNnK5McurY7Y6OLB8duKlJrVFzUlgFFieIQTc1"
-access_secret = "Vikvgo4Mpn7cUUOi2Dp4yv6yqsZPCDsmVAst4plmnoxcQ"
+class Tweet_Object:
+    def __init__(self, tweetObject):
+        self.tweet_object = tweetObject
+        tweetDict = json.loads(json.dumps(tweetObject._json))
+        self.tweet_id = tweetDict["id_str"]
+        self.created_at = tweetDict["created_at"]
+        self.text = tweetDict["text"]
+        self.user_id = tweetDict["user"]["id_str"]
+        self.user_name = tweetDict["user"]["screen_name"]
+        self.user_verified = tweetDict["user"]["verified"]
+        self.retweet_count = tweetDict["retweet_count"]
+        self.favorite_count = tweetDict["favorite_count"]
 
-api = twitter.Api(consumer_key=consumer_key,
-                  consumer_secret=consumer_secret,
-                  access_token_key=access_token,
-                  access_token_secret=access_secret)
+def get_twitter_data(query):
 
-# query settings
-query = "Donald Trump"
-result_type = "mixed"
-geocode = ""
+    tweet_list = []
+    id_list = []
+    # twitter api setup
+    consumer_key = "8kpMQ4bOrZyse7UIZwWbnFnkv"
+    consumer_secret = "x2mUXcR3CtuYpZ62xnspwUezaqMSPGiy9GKAuSlydkv717h3kD"
+    access_token = "3044703939-YbNnK5McurY7Y6OLB8duKlJrVFzUlgFFieIQTc1"
+    access_secret = "Vikvgo4Mpn7cUUOi2Dp4yv6yqsZPCDsmVAst4plmnoxcQ"
 
+    api = twitter.Api(consumer_key=consumer_key,
+                      consumer_secret=consumer_secret,
+                      access_token_key=access_token,
+                      access_token_secret=access_secret)
 
-results = api.GetSearch(
-    raw_query="q=%s&result_type=%s&count=100" % (query, result_type))
-tweet = results[0]
-json_str = json.dumps(tweet._json)
-print(json.loads(json_str)["created_at"])
+    result_types = ["mixed", "popular", "recent"]
+    for query_type in result_types:
+        results = api.GetSearch(
+            raw_query="q=%s&result_type=%s&count=100" % (query, query_type))
+
+        for tweetData in results:
+            tweet = Tweet_Object(tweetData)
+            if tweet.user_id not in id_list:
+                tweet_list.append(tweet)
+                id_list.append(tweet.tweet_id)
+
+    return tweet_list
