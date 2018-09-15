23456ccdebdf08bf203a75a38e2919f8dfd16f93
arifaulakh <aular@utschools.ca>
2018-09-15 14:25:27 -0400

    add/change search files

--- mysite/search/views.py
+++ mysite/search/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from twitter_pulls import Tweet_Object, get_twitter_data
 # Create your views here.
 def search_form(request):
     return render(request,'search/search_form.html')
